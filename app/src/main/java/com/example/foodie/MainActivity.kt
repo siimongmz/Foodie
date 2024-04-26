62c5188e9bf3ed5ebef8528e51900bc547fe31c5
siimongmz <118911662+siimongmz@users.noreply.github.com>
2024-04-26 14:13:26 +0200

    Ahora los produtos se crearan usando un ProductInfo

--- app/src/main/java/com/example/foodie/MainActivity.kt
+++ app/src/main/java/com/example/foodie/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.foodie
 
-import android.icu.text.CaseMap.Title
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,7 +7,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -31,7 +29,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -41,6 +39,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.foodie.ui.theme.FoodieTheme
+import com.example.foodie.ui.theme.primaryDark
 
 data class BottomItem(
     val title: String,
@@ -50,54 +49,50 @@ data class BottomItem(
     )
 
 data class ProductInfo(
-    val name: String,
-    val comapny: String,
-    val color: Color
+    val name: String, val company: String, val color: Color
 )
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val items = listOf(
+            BottomItem(
+                title = "Share",
+                selectedIcon = Icons.Filled.Share,
+                unselectedIcon = Icons.Outlined.Share
+            ), BottomItem(
+                title = "Home",
+                selectedIcon = Icons.Filled.Home,
+                unselectedIcon = Icons.Outlined.Home
+            ), BottomItem(
+                title = "Settings",
+                selectedIcon = Icons.Filled.Settings,
+                unselectedIcon = Icons.Outlined.Settings
+            )
+        )
         super.onCreate(savedInstanceState)
         setContent {
 
             FoodieTheme {
-
-                var items = listOf(
-                    BottomItem(
-                        title = "Share",
-                        selectedIcon = Icons.Filled.Share,
-                        unselectedIcon = Icons.Outlined.Share
-                    ), BottomItem(
-                        title = "Home",
-                        selectedIcon = Icons.Filled.Home,
-                        unselectedIcon = Icons.Outlined.Home
-                    ), BottomItem(
-                        title = "Settings",
-                        selectedIcon = Icons.Filled.Settings,
-                        unselectedIcon = Icons.Outlined.Settings
-                    )
-                )
 
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     Scaffold(bottomBar = {
 
-                        NavigationBar {
+                        NavigationBar (Modifier.background(primaryDark)) {
                             var selectedIndex by rememberSaveable {
-                                mutableStateOf(1)
+                                mutableIntStateOf(1)
                             }
 
                             items.forEachIndexed { index, unit ->
-                                NavigationBarItem(
-                                    label = { Text(text = unit.title) },
+                                NavigationBarItem(label = { Text(text = unit.title) },
                                     selected = index == selectedIndex,
                                     onClick = { selectedIndex = index },
                                     icon = {
-                                        if (index == selectedIndex) Icon(
-                                            unit.selectedIcon,
-                                            unit.title
-                                        ) else Icon(unit.unselectedIcon, unit.title)
+                                        if (index == selectedIndex)
+                                            Icon(unit.selectedIcon, unit.title)
+                                        else
+                                            Icon(unit.unselectedIcon, unit.title)
                                     })
                             }
 
@@ -113,7 +108,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Product(name: String, comapany: String, color: Color, modifier: Modifier = Modifier) {
+fun Product(info: ProductInfo, modifier: Modifier = Modifier) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Start,
@@ -123,16 +118,16 @@ fun Product(name: String, comapany: String, color: Color, modifier: Modifier = M
             modifier
                 .width(60.dp)
                 .height(60.dp)
-                .background(color)
+                .background(info.color)
         )
         Column(horizontalAlignment = Alignment.Start) {
             Text(
-                text = name,
+                text = info.name,
                 style = MaterialTheme.typography.titleMedium,
                 modifier = modifier.padding(20.dp, 0.dp)
             )
             Text(
-                text = comapany,
+                text = info.company,
                 style = MaterialTheme.typography.titleSmall,
                 modifier = modifier.padding(20.dp, 0.dp),
                 color = Color.Gray
@@ -147,7 +142,7 @@ fun Product(name: String, comapany: String, color: Color, modifier: Modifier = M
 @Composable
 fun ProductList(modifier: Modifier = Modifier) {
     val listProductInfo = listOf(
-        ProductInfo("Aquarius", "Cocacola Company", Color.Cyan),
+        ProductInfo("Aquarius", "Coca-cola Company", Color.Cyan),
         ProductInfo("Nestea", "Cocacola Company", Color.Blue),
         ProductInfo("Lipton", "Pepsi Company", Color.Yellow),
         ProductInfo("Lays", "Pepsi Company", Color.Red),
@@ -155,9 +150,9 @@ fun ProductList(modifier: Modifier = Modifier) {
     )
     LazyColumn {
 
-        listProductInfo.forEachIndexed { index, productInfo ->
+        listProductInfo.forEach { productInfo ->
             item {
-                Product(productInfo.name, productInfo.comapny, productInfo.color)
+                Product(productInfo)
             }
         }
     }
