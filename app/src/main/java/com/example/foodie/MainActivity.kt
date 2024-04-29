c406f5b2e81a1110b1e60dbff0b542af050c5239
siimongmz <118911662+siimongmz@users.noreply.github.com>
2024-04-29 09:57:02 +0200

    Agregada Barra Superior, FAB, edge to edge para pintar en toda la pantalla

--- app/src/main/java/com/example/foodie/MainActivity.kt
+++ app/src/main/java/com/example/foodie/MainActivity.kt
@@ -3,30 +3,41 @@ package com.example.foodie
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -35,8 +46,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import com.example.foodie.ui.theme.FoodieTheme
 import com.example.foodie.ui.theme.primaryDark
@@ -54,6 +69,9 @@ data class ProductInfo(
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+
+
         val items = listOf(
             BottomItem(
                 title = "Share",
@@ -73,13 +91,15 @@ class MainActivity : ComponentActivity() {
         setContent {
 
             FoodieTheme {
-
+                window.statusBarColor = MaterialTheme.colorScheme.secondaryContainer.toArgb()
                 Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.primaryContainer
                 ) {
+
                     Scaffold(bottomBar = {
 
-                        NavigationBar (Modifier.background(primaryDark)) {
+                        NavigationBar() {
                             var selectedIndex by rememberSaveable {
                                 mutableIntStateOf(1)
                             }
@@ -89,22 +109,54 @@ class MainActivity : ComponentActivity() {
                                     selected = index == selectedIndex,
                                     onClick = { selectedIndex = index },
                                     icon = {
-                                        if (index == selectedIndex)
-                                            Icon(unit.selectedIcon, unit.title)
-                                        else
-                                            Icon(unit.unselectedIcon, unit.title)
+                                        if (index == selectedIndex) Icon(
+                                            unit.selectedIcon, unit.title
+                                        )
+                                        else Icon(unit.unselectedIcon, unit.title)
                                     })
                             }
 
                         }
-                    }) { paddingValues ->
+                    },
+                                floatingActionButton = { ScannerFAB() },
+                                topBar = { TopBar() },
+                                contentWindowInsets = WindowInsets.safeDrawing)
+                    { paddingValues ->
                         ProductList(Modifier.padding(paddingValues))
+                        window.statusBarColor = MaterialTheme.colorScheme.secondaryContainer.toArgb()
+
                     }
 
                 }
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(modifier: Modifier = Modifier) {
+    TopAppBar(
+        title = {
+
+            Text(
+                text = "Foodie",
+                letterSpacing = TextUnit(5F, TextUnitType.Sp),
+                fontWeight = FontWeight.SemiBold
+            )
+        }, colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.secondary,
+        ), navigationIcon = {
+            Icon(
+                imageVector = Icons.Outlined.Search,
+                contentDescription = "Search",
+                modifier = Modifier.padding(10.dp, 10.dp),
+                tint = MaterialTheme.colorScheme.primary
+            )
+        }
+
+    )
 }
 
 @Composable
@@ -140,6 +192,18 @@ fun Product(info: ProductInfo, modifier: Modifier = Modifier) {
 
 @Preview
 @Composable
+fun ScannerFAB(modifier: Modifier = Modifier) {
+    LargeFloatingActionButton(
+        onClick = { /*TODO*/ },
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentColor = MaterialTheme.colorScheme.onPrimary
+    ) {
+        Icon(Icons.Outlined.Add, "Add")
+    }
+}
+
+@Preview
+@Composable
 fun ProductList(modifier: Modifier = Modifier) {
     val listProductInfo = listOf(
         ProductInfo("Aquarius", "Coca-cola Company", Color.Cyan),
@@ -147,8 +211,19 @@ fun ProductList(modifier: Modifier = Modifier) {
         ProductInfo("Lipton", "Pepsi Company", Color.Yellow),
         ProductInfo("Lays", "Pepsi Company", Color.Red),
         ProductInfo("Patatas Campesinas", "Hacendado", Color.Green),
-    )
-    LazyColumn {
+        ProductInfo("Aquarius", "Coca-cola Company", Color.Cyan),
+        ProductInfo("Nestea", "Cocacola Company", Color.Blue),
+        ProductInfo("Lipton", "Pepsi Company", Color.Yellow),
+        ProductInfo("Lays", "Pepsi Company", Color.Red),
+        ProductInfo("Patatas Campesinas", "Hacendado", Color.Green),
+        ProductInfo("Aquarius", "Coca-cola Company", Color.Cyan),
+        ProductInfo("Nestea", "Cocacola Company", Color.Blue),
+        ProductInfo("Lipton", "Pepsi Company", Color.Yellow),
+        ProductInfo("Lays", "Pepsi Company", Color.Red),
+        ProductInfo("Patatas Campesinas", "Hacendado", Color.Green),
+
+        )
+    LazyColumn(Modifier.imePadding()) {
 
         listProductInfo.forEach { productInfo ->
             item {
@@ -157,3 +232,4 @@ fun ProductList(modifier: Modifier = Modifier) {
         }
     }
 }
+
