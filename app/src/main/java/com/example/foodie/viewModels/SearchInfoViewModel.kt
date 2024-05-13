7ea11f9f1ee488544375bbc4ace358ff9ef4cc32
siimongmz <118911662+siimongmz@users.noreply.github.com>
2024-05-13 16:26:11 +0200

    Se agrega una lista de productos recientes al viewModel

--- app/src/main/java/com/example/foodie/viewModels/SearchInfoViewModel.kt
+++ app/src/main/java/com/example/foodie/viewModels/SearchInfoViewModel.kt
@@ -3,8 +3,10 @@ package com.example.foodie.viewModels
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.foodie.api.data.JsonFoodItem
+import java.util.Collections
 
 class SearchInfoViewModel : ViewModel() {
     var currentProduct = mutableStateOf<JsonFoodItem?>(null)
     var code = mutableStateOf<String?>(null)
+    var recentProducts = mutableStateOf<List<JsonFoodItem>>(Collections.emptyList<JsonFoodItem>())
 }
