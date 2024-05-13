bfa4ea112dd380124f3649ce1f725547d58c4dc4
siimongmz <118911662+siimongmz@users.noreply.github.com>
2024-05-13 16:25:46 +0200

    En la pantalla principal aparecerá la busqueda y se mostrará la lista en dos columnas

--- app/src/main/java/com/example/foodie/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/foodie/ui/screens/MainScreen.kt
@@ -5,13 +5,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -29,6 +29,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -39,17 +40,26 @@ data class ProductInfo(
 )
 
 @Composable
-fun MainScreen(modifier: Modifier,searchInfoViewModel: SearchInfoViewModel) {
-    ProductList(modifier)
+fun MainScreen(modifier: Modifier, searchInfoViewModel: SearchInfoViewModel) {
+    Column(modifier = modifier) {
+        SearchScreen(searchInfoViewModel = searchInfoViewModel)
+        ProductList()
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Product(info: ProductInfo, modifier: Modifier = Modifier, onItemClick:() -> Unit) {
-    Card (onClick = onItemClick, colors = CardDefaults.cardColors(MaterialTheme.colorScheme.background)){
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start,
+fun Product(info: ProductInfo, modifier: Modifier = Modifier, onItemClick: () -> Unit) {
+    Card(
+        onClick = onItemClick,
+        modifier = Modifier.padding(10.dp),
+        colors = CardDefaults.cardColors(
+            MaterialTheme.colorScheme.background,
+        )
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .padding(10.dp, 10.dp)
                 .fillMaxWidth()
@@ -60,18 +70,19 @@ fun Product(info: ProductInfo, modifier: Modifier = Modifier, onItemClick:() -> 
                     .width(60.dp)
                     .height(60.dp)
                     .background(info.color)
+
             )
-            Column(horizontalAlignment = Alignment.Start) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(top = 5.dp)) {
                 Text(
                     text = info.name,
                     style = MaterialTheme.typography.titleMedium,
-                    modifier = modifier.padding(20.dp, 0.dp)
+                    textAlign = TextAlign.Center
                 )
                 Text(
                     text = info.company,
                     style = MaterialTheme.typography.titleSmall,
-                    modifier = modifier.padding(20.dp, 0.dp),
-                    color = Color.Gray
+                    color = Color.Gray,
+                    textAlign = TextAlign.Center
                 )
             }
 
@@ -106,16 +117,18 @@ fun ProductList(modifier: Modifier = Modifier) {
     }
     val lazyListState = rememberLazyListState()
     var clickedItem by remember { mutableStateOf<ProductInfo?>(null) }
-    LazyColumn(state = lazyListState, modifier = Modifier.imePadding()) {
-
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(minSize = 128.dp),
+        modifier = Modifier.imePadding()
+    ) {
         listProductInfo.forEach { productInfo ->
-            item { Spacer(modifier = Modifier.height(10.dp)) }
-            item {
 
-                Product(productInfo, onItemClick =  { clickedItem = productInfo })
+            item {
+                Product(productInfo, onItemClick = { clickedItem = productInfo })
 
             }
         }
+
     }
     clickedItem?.let {
         ModalBottomSheet(
