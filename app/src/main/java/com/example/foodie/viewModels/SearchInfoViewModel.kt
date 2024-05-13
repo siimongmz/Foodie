9890adf883b81e6bcfb81cbb0210befcd3ea287d
siimongmz <118911662+siimongmz@users.noreply.github.com>
2024-05-13 17:06:27 +0200

    Agregada la lista en la que se almacenan los productos recientes

--- app/src/main/java/com/example/foodie/viewModels/SearchInfoViewModel.kt
+++ app/src/main/java/com/example/foodie/viewModels/SearchInfoViewModel.kt
@@ -1,12 +1,12 @@
 package com.example.foodie.viewModels
 
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.ViewModel
 import com.example.foodie.api.data.JsonFoodItem
-import java.util.Collections
 
 class SearchInfoViewModel : ViewModel() {
     var currentProduct = mutableStateOf<JsonFoodItem?>(null)
     var code = mutableStateOf<String?>(null)
-    var recentProducts = mutableStateOf<List<JsonFoodItem>>(Collections.emptyList<JsonFoodItem>())
+    var recentProducts = SnapshotStateList<JsonFoodItem>()
 }
