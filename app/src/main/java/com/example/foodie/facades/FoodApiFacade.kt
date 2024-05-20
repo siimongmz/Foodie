a5a346b5e722b04d80839b9f41bdea61af31e79b
siimongmz <118911662+siimongmz@users.noreply.github.com>
2024-05-20 09:10:04 +0200

    Ahora la Facade hará uso de un unico objeto de tipo FoodApi

--- app/src/main/java/com/example/foodie/facades/FoodApiFacade.kt
+++ app/src/main/java/com/example/foodie/facades/FoodApiFacade.kt
@@ -4,10 +4,9 @@ import com.example.foodie.api.FoodApi
 import com.example.foodie.api.data.FoodItem
 
 class FoodApiFacade {
+    private val foodApi = FoodApi()
 
-    fun getProduct(code:String): FoodItem? {
-        val foodApi = FoodApi()
-
+    fun getProduct(code: String): FoodItem? {
         return foodApi.getProduct(code)
 
     }
