918a86b6a29fe737afe120ffd19224116c7f6e60
siimongmz <118911662+siimongmz@users.noreply.github.com>
2024-05-20 09:06:36 +0200

    Agregada la alergia al pescado

--- app/src/main/java/com/example/foodie/configuration/ALLERGENS.kt
+++ app/src/main/java/com/example/foodie/configuration/ALLERGENS.kt
@@ -1,6 +1,6 @@
 package com.example.foodie.configuration
 
 enum class ALLERGENS {
-    EGG,MILK,GLUTEN,SOYBEANS,NUTS
+    EGG,MILK,GLUTEN,SOYBEANS,NUTS,FISH
 }
 data class Allergen(val allergen: ALLERGENS, val name:String)
--- app/src/main/java/com/example/foodie/tools/Translator.kt
+++ app/src/main/java/com/example/foodie/tools/Translator.kt
@@ -1,12 +1,13 @@
 package com.example.foodie.tools
 
-fun translate(text:String):String{
-    return when(text){
+fun translate(text: String): String {
+    return when (text) {
         "eggs" -> "Huevo"
         "milk" -> "Leche"
         "gluten" -> "Gluten"
         "soybeans" -> "Soja"
         "nuts" -> "Nueces"
+        "fish" -> "Pescado"
         else -> "Sin Traduccion: $text"
 
     }
