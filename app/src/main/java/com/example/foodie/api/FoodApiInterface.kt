509acba15079d2b298477ef749d5a104a50565ce
siimongmz <118911662+siimongmz@users.noreply.github.com>
2024-05-28 14:32:03 +0200

    Now is forced to give a JSON as a default response

--- app/src/main/java/com/example/foodie/api/FoodApiInterface.kt
+++ app/src/main/java/com/example/foodie/api/FoodApiInterface.kt
@@ -7,6 +7,6 @@ import retrofit2.http.Path
 
 interface FoodApiInterface {
 
-    @GET("product/{code}?lc=es&?fields=product_name,ingredients,brands,allergens,image_front_small_url,image_ingredients_url")
+    @GET("product/{code}?lc=es&?fields=product_name,ingredients,brands,allergens,image_front_small_url,image_ingredients_url.json")
     fun getData(@Path("code") codeId: String): Call<FoodItem>
 }
