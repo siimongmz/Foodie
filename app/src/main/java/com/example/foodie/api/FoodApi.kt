b428413e309a7c1eacd9a9855db741f3251eabee
siimongmz <118911662+siimongmz@users.noreply.github.com>
2024-05-28 14:31:28 +0200

    The response is saved now in the logs and the api url has changed from .net to .org

--- app/src/main/java/com/example/foodie/api/FoodApi.kt
+++ app/src/main/java/com/example/foodie/api/FoodApi.kt
@@ -1,13 +1,14 @@
 package com.example.foodie.api
 
 
+import android.util.Log
 import com.example.foodie.api.data.FoodItem
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class FoodApi {
     companion object {
-        private const val BASE_URL = "https://world.openfoodfacts.net/api/v2/"
+        private const val BASE_URL = "https://world.openfoodfacts.org/api/v2/"
     }
 
     fun getProduct(code: String): FoodItem? {
@@ -19,7 +20,7 @@ class FoodApi {
                 .create(FoodApiInterface::class.java)
         val retrofitData = retrofitBuilder.getData(code)
 
-        return retrofitData.execute().body()
+        return retrofitData.execute().body().also { Log.d("Respuesta",it.toString()) }
 
 
     }
